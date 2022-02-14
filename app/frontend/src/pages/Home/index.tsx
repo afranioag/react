415effc2c4a9a056214926d2201c7689591ee2e2
afranio.garcia <afranio.cet@gmail.com>
2022-02-14 17:45:36 -0300

    button

--- app/frontend/src/pages/Home/index.tsx
+++ app/frontend/src/pages/Home/index.tsx
@@ -1,26 +1,31 @@
 import NavBar from "components/NavBar";
-import { ReactComponent as MainImage} from 'assets/images/main-image.svg'
+import { ReactComponent as MainImage } from "assets/images/main-image.svg";
+import ButtonIcon from "components/ButtonIcon";
+import "./styles.css";
+
 const Home = () => {
-    return(
-        <>
-        <NavBar />
-        <div className="home-container">
-            <div className="home-card">
-                <div className="home-content-container">
-                    <h1>
-                        Conheça o melhor catálogo de conteúdo
-                    </h1>
-
-                </div>
-                <div className="home-image-container">
-                    <MainImage />
-                </div>
-
+  return (
+    <>
+      <NavBar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catálogo de conteúdo</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
             </div>
-
+            <ButtonIcon />
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Home;
