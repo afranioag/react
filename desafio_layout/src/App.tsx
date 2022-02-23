3176691760362b25cee73e656cfb4a18251537b6
afranio.garcia <afranio.cet@gmail.com>
2022-02-23 20:05:45 -0300

    novas alterações

--- desafio_layout/src/App.tsx
+++ desafio_layout/src/App.tsx
@@ -1,9 +1,14 @@
+import NavBar from 'components/NavBar';
 import './assets/styles/custom.scss';
 import './App.css';
 
+
 const App = () => {
   return (
+    <>
+    <NavBar />
     <h1>ESTOU LIMPO, E PRONTO PARA COMEÇAR!</h1>
+    </>
   );
 }
 
